d3ce2e813b48edf6c36390b8bbfbaf8d2bab3dbe
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-06 11:07:08 -0300

    controlar proproiedades css baseado em props que o componente recebe

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 
 export const Title = styled.h1`
     font-size: 24px;
-    color: #7159c1;
+    color: ${props => (props.error ? 'red' : '#7159c1')};
     font-family: Arial, Helvetica, sans-serif;
 `;
+// controlar proproiedades css baseado em props que o componente recebe
 
