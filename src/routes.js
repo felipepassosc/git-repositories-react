6c0d00fa9e98b2863b64bc6fbf2c0bd3205e907f
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-06 10:02:54 -0300

    BrowserRouter permite a navegação, fica com uma barra /main / repo

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+// BrowserRouter permite a navegação, fica com uma barra /main / repo
+
+import React from 'react';
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+
+        </BrowserRouter>
+    )
+}
+
+// Switch - Uma rota seja chamada por vez
+
+export default Routes;
