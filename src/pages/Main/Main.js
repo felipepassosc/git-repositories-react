ebc1efb4431e47c9643a0edd3e6caea9566d5686
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-06 12:13:14 -0300

    // quando preciso estilizar um elemento baseado em alguma propriedade, criar componentes

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt } from 'react-icons/fa'
+import { Container } from './styles';
 
 function Main() {
     return (
-    <Title error={false}>
-        Main
-    </Title>
+        <Container>
+            <h1>
+                <FaGithubAlt />
+                Repositórios
+             </h1>
+            <Form onSubmit={() => { }}>
+                <input
+                    type="text"
+                    placeholder="Adicionar Repositório"
+                />
+            </Form>
+        </Container>
     )
 }
 
