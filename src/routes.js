ef8d5b7543c73390a326c31cd878f1078a5c0b9c
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-06 10:12:33 -0300

    Route - nossas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,21 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 // BrowserRouter permite a navegação, fica com uma barra /main / repo
+// Switch - Uma rota seja chamada por vez
+// Nossas rotas
 
 import React from 'react';
-import Main from './pages/Main';
-import Repository from './pages/Repository';
+import Main from './pages/Main/Main';
+import Repository from './pages/Repository/Repository';
 
 function Routes() {
     return (
         <BrowserRouter>
-
+            <Switch>
+                <Route path="/" component={Main}></Route>
+                <Route path="/repository" component={Repository}></Route>
+            </Switch>
         </BrowserRouter>
     )
 }
-
-// Switch - Uma rota seja chamada por vez
 
 export default Routes;
