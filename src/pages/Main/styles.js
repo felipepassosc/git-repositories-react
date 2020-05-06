66a8b1c0c4f185ff79a163f838a75cca6829fe5a
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-06 10:53:45 -0300

    componentes-estilzados
    cada arquivo css tem seu proprio escopo, pertence apenas aquele componente

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+// cada arquivo css tem seu proprio escopo, pertence apenas aquele componente
+
+export const Title = styled.h1`
+    font-size: 24px;
+    color: #7159c1;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
