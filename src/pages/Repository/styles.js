8ded3427076589c9af892abeed9a4db9be3993de
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-07 11:48:25 -0300

    estilizando owner

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -9,3 +9,24 @@ export const Loading = styled.div`
     align-items: center;
     height: 100vh;
 `;
+
+export const Owner = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+      width: 120px;
+      border-radius: 50%;
+      margin-top: 20px;
+  }
+
+  p {
+      margin-top: 5px;
+      font-size: 16px;
+      color: #666;
+      line-height: 1.4;
+      text-align: center;
+      max-width: 400px;
+  }
+`;
