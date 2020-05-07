b0c037438958bb3a439aa8e7f8c66a907c1ed6ce
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-07 11:34:06 -0300

    add proptypes pra defenir os tipos e usando container

--- src/pages/Repository/Repository.js
+++ src/pages/Repository/Repository.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import api from '../../services/api'
-// import { Container } from './styles';
+import PropTypes from 'prop-types'
+import { Loading } from './styles';
+import Container from '../../Components/Container/Container'
 
 class Repository extends Component {
-    async componentDidMount(){
+    static propTypes = {
+        match: PropTypes.shape({
+            params: PropTypes.shape({
+                repository: PropTypes.string,
+            })
+        }).isRequired,
+    }
+
+    state = {
+        repository: {},
+        issues: [],
+        loading: true
+    }
+
+    // Mudei para um componentede classe, usei o component como assincrono para poder sar a api do github
+    async componentDidMount() {
         const { match } = this.props
 
         const repoName = decodeURIComponent(match.params.repository)
@@ -12,14 +29,28 @@ class Repository extends Component {
 
         const [repository, issues] = await Promise.all([
             api.get(`/repos/${repoName}`),
-            api.get(`/repos/${repoName}/issues`),
+            api.get(`/repos/${repoName}/issues`, {
+                params: {
+                    state: 'open',
+                    per_page: 5
+                },
+            }),
         ])
+        // parametros só os que estão aberto e aprnas 5 issues
 
-        console.log(repository)
-        console.log(issues)
+        this.setState({
+            repository: repository.data,
+            issues: issues.data,
+            loading: false
+        })
     }
 
-    render(){
+    render() {
+        const { repository, issues, loading } = this.state
+
+        if (loading) {
+            return <Loading>Carregando...</Loading>
+        }
         return <h1>Repository</h1>
     }
 }
